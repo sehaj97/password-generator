23537695780128dac5e0bf3eec985357107694f2
sehaj97 <sehajmagan@gmail.com>
2021-09-12 13:51:24 -0400

    added charater types prompt

--- Develop/script.js
+++ Develop/script.js
@@ -2,20 +2,39 @@
   var msg = document.getElementById("msg");
   var passwordLengthBtn = document.getElementById("btnlen");
   var passwordLengthText = document.getElementById("plen");
+  var passwordCheckCharBtn = ""
   
   passwordLengthBtn.addEventListener("click", checkLength);
 
-function checkLength() {
-  msg.innerText = "";
-  if(passwordLengthText.value < 8 || passwordLengthText.value > 128){
-    msg.innerText = "length should be between 8-128 characters";s
-  } else {
+  function checkLength() {
     msg.innerText = "";
-    passwordLengthText.style.backgroundColor = "green";
-    passwordLengthText.disabled = true;
-    document.getElementById("charTypes").style.display = "block";
+    if(passwordLengthText.value < 8 || passwordLengthText.value > 128){
+      msg.innerText = "length should be between 8-128 characters";s
+    } else {
+      msg.innerText = "";
+      passwordLengthText.style.backgroundColor = "green";
+      passwordLengthText.disabled = true;
+      document.getElementById("charTypes").style.display = "block";
+      passwordLengthBtn.id= "checkChar";
+      passwordCheckCharBtn = document.getElementById("checkChar");
+      passwordCheckCharBtn.addEventListener("click", createPassword);
+    }
   }
-}
+
+  function createPassword() {
+    msg.innerText = "I work";
+  }
+
+  function generatePassword() {
+    msg.innerText = "";
+    passwordLengthText.value="8"
+    passwordLengthText.style.backgroundColor = "white";
+    passwordLengthText.disabled = false;
+    document.getElementById("charTypes").style.display = "none";
+    passwordCheckCharBtn.id="btnlen"
+    passwordLengthBtn.addEventListener("click", checkLength);
+    return ""
+  }
 
 
 // Get references to the #generate element
@@ -23,15 +42,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = '';
-  //generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = password;
   //display prompts for password
   var passwordPrompts = document.getElementById("password-prompts");
-  
-  //checkPrompts();
-
-  passwordText.value = password;
   passwordPrompts.style.display = 'flex';
 }
 
