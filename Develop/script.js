d0352ff4028810aaf408bb0a2c5b297fa065b15b
sehaj97 <sehajmagan@gmail.com>
2021-09-12 14:39:11 -0400

    check for character  prompt

--- Develop/script.js
+++ Develop/script.js
@@ -2,31 +2,65 @@
   var msg = document.getElementById("msg");
   var passwordLengthBtn = document.getElementById("btnlen");
   var passwordLengthText = document.getElementById("plen");
-  var passwordCheckCharBtn = ""
+  var passwordCheckCharBtn = document.getElementById("chkChar");
+  var pass = "";
   
   passwordLengthBtn.addEventListener("click", checkLength);
+  passwordCheckCharBtn.addEventListener("click", createPassword);
 
   function checkLength() {
-    msg.innerText = "";
+    msg.style.display = "none";
     if(passwordLengthText.value < 8 || passwordLengthText.value > 128){
-      msg.innerText = "length should be between 8-128 characters";s
+      msg.style.display = "block";
+      msg.innerText = "length should be between 8-128 characters";
     } else {
-      msg.innerText = "";
+      msg.style.display = "none";
       passwordLengthText.style.backgroundColor = "green";
       passwordLengthText.disabled = true;
       document.getElementById("charTypes").style.display = "block";
-      passwordLengthBtn.id= "checkChar";
-      passwordCheckCharBtn = document.getElementById("checkChar");
-      passwordCheckCharBtn.addEventListener("click", createPassword);
+      passwordLengthBtn.style.display = "none"
+      passwordCheckCharBtn.style.display = "block"
     }
   }
 
-  function createPassword() {
-    msg.innerText = "I work";
+  function createPassword() { 
+    var checkedCounter = 0;
+    var numberChars = "";
+    var upperChars = "";
+    var lowerChars = "";
+    var specialChars = "";
+    var inputElements = document.getElementsByClassName('charCheckbox');
+    for(var i=0; inputElements[i]; ++i){
+      if(inputElements[i].checked){
+           checkedCounter++;
+      }
+    }
+    if (checkedCounter !== 0){
+      msg.style.display = "none";
+      if(document.querySelector("#numeric").checked){
+        numberChars = "0123456789";
+      }
+      if(document.querySelector("#uppercase").checked){
+        upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      }
+      if(document.querySelector("#lowercase").checked){
+        lowerChars = "abcdefghijklmnopqrstuvwxyz";
+      }
+      if(document.querySelector("#specialChars").checked){
+        specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+      }
+      //pass = getPassword(numberChars,upperChars,lowerChars,specialChars);
+
+    } else {
+      msg.style.display = "block";
+      msg.innerText = "Please Select atleast one character type";
+    }
   }
 
   function generatePassword() {
-    msg.innerText = "";
+    msg.innerText = pass;
+    passwordLengthBtn.style.display = "block"
+    passwordCheckCharBtn.style.display = "none"
     passwordLengthText.value="8"
     passwordLengthText.style.backgroundColor = "white";
     passwordLengthText.disabled = false;
