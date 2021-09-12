9c39ea2732397c1fe8b0908be057d2e746a7c266
sehaj97 <sehajmagan@gmail.com>
2021-09-12 13:09:38 -0400

    added styles and check for length

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,21 @@
 // Assignment code here
+  var msg = document.getElementById("msg");
+  var passwordLengthBtn = document.getElementById("btnlen");
+  var passwordLengthText = document.getElementById("plen");
+  
+  passwordLengthBtn.addEventListener("click", checkLength);
+
+function checkLength() {
+  msg.innerText = "";
+  if(passwordLengthText.value < 8 || passwordLengthText.value > 128){
+    msg.innerText = "length should be between 8-128 characters";s
+  } else {
+    msg.innerText = "";
+    passwordLengthText.style.backgroundColor = "green";
+    passwordLengthText.disabled = true;
+    document.getElementById("charTypes").style.display = "block";
+  }
+}
 
 
 // Get references to the #generate element
@@ -12,6 +29,8 @@ function writePassword() {
   //display prompts for password
   var passwordPrompts = document.getElementById("password-prompts");
   
+  //checkPrompts();
+
   passwordText.value = password;
   passwordPrompts.style.display = 'flex';
 }
